41a785a03fba7a928afd1a38ca44cf76907e6e30
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2020-09-29 13:26:18 +0200

    Complete basic jetpack tutorial

--- ComposeTutorial/app/src/main/java/com/alexskvor/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/alexskvor/composetutorial/MainActivity.kt
@@ -2,37 +2,76 @@ package com.alexskvor.composetutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import com.alexskvor.composetutorial.ui.ComposeTutorialTheme
+import com.alexskvor.composetutorial.ui.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        MaterialTheme {
+
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp))
+
+            Image(
+                asset = image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            Text(
+                text = "A very long title for article" +
+                        " that describes the full content" +
+                        " of article itself and more then that " +
+                        "this title is so long that we need " +
+                        "to crop it with two lines or even less",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Text 22222222222",
+                style = typography.body2
+            )
+            Text(
+                text = "Text 33333333333",
+                style = typography.body2
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewGreeting() {
+    NewsStory()
 }
